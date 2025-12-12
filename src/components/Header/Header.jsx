3dee42d91a3b3c5f425e9400956ac1c12bc4cd89
Nikolai Gavriushin <gna@pm.me>
2025-12-12 18:54:02 +0400

    feat(header): add links to profile and main pages

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,31 +3,47 @@ import "./Header.css";
 import logoImage from "../../images/logo.svg";
 import avatarImage from "../../images/avatar.png";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
+import { NavLink } from "react-router-dom";
 
 function Header({ handleAddClick, currentLocation }) {
-
   const currentDate = new Date().toLocaleString("default", {
     month: "long",
     day: "numeric",
   });
 
-  return(
+  return (
     <header className="header">
       <div className="header__container">
-        <img src={logoImage} alt="Logo" className="header__logo" />
-        <p className="header__date">{currentDate}, {currentLocation}</p>
+        <NavLink to="/">
+          <img src={logoImage} alt="Logo" className="header__logo" />
+        </NavLink>
+        <p className="header__date">
+          {currentDate}, {currentLocation}
+        </p>
       </div>
       <nav className="header__nav">
-        < ToggleSwitch />
-        <button type="button" className="header__button" onClick={handleAddClick}>+ Add clothes</button>
-        <div className="header__profile">
-          <p className="header__username">Terrence Tegegne</p>
-          <img src={avatarImage} alt="User avatar" className="header__avatar" />
-        </div>
+        <ToggleSwitch />
+        <button
+          type="button"
+          className="header__button"
+          onClick={handleAddClick}
+        >
+          + Add clothes
+        </button>
+        <NavLink to="/profile" className="header__link">
+          <div className="header__profile">
+            <p className="header__username">Terrence Tegegne</p>
+            <img
+              src={avatarImage}
+              alt="User avatar"
+              className="header__avatar"
+            />
+          </div>
+        </NavLink>
       </nav>
       <button className="header__mobile-menu"></button>
     </header>
-  )
+  );
 }
 
 export default Header;
